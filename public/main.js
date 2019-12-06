ac741667b4f84a9625b5d7626d667e146eaa9906
Law <michaellaw@gmx.com>
2019-12-06 14:47:47 -0500

    Used the FormData web api and Fetch api to submit a request to our server via an IIFE + async within an event listener that cancels the default behavior of the form

--- public/main.js
+++ public/main.js
@@ -2,7 +2,7 @@
 *  load the file via AJAX, might run into CORS though
 *
 */
-
+let newPokemonForm = document.getElementById('new-pokemon')
 let slot = document.getElementById('slot')
 let loadButton = document.getElementById('load-btn')
 
@@ -20,3 +20,23 @@ console.log(res)
     <h4>${res.exp} EXP.</h4>
   `
 }
+
+newPokemonForm.addEventListener('submit', (e) => {
+  (async () => {
+    e.preventDefault()
+    let name = document.getElementById('pokemon-name').value
+    // take in name paramter from from
+    let req = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    let res = await req.json()
+    console.log(res)
+    let form = new FormData()
+    form.append('pokemon_name', name) 
+    let req2 = await fetch('/pokemon', {method: 'POST', body: form})
+    let res2 = await req2.json()
+    console.log(res2)
+  })()  
+})
+
+
+
+
--- server.rb
+++ server.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
+require './pokemon.rb'
 set :port, 3000
 
 get '/' do
   erb :index
+end
+
+
+post '/pokemon' do
+  puts params
+  pokemon = Pokemon.new(params['pokemon_name'])
+  pokemon.save
+  return pokemon.serialize
 end
